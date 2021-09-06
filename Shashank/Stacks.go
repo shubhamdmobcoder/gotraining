257eae6922581ff2a8ee511cc9212862e9988a75
Shashank-Mobcoder <90186257+Shashank-Mobcoder@users.noreply.github.com>
2021-09-06 17:00:00 +0530

    Update Stacks.go

--- Shashank/Stacks.go
+++ Shashank/Stacks.go
@@ -4,30 +4,28 @@ import "fmt"
 
 type Stack []string
 
-// Checking if stack is empty
 func (newStack *Stack) IsEmpty() bool {
 	return len(*newStack) == 0
 }
 
-// Pushing a new value into the stack
 func (newStack *Stack) Push(str string) {
-	*newStack = append(*newStack, str) //  Appending the new value to the end of the stack
+	*newStack = append(*newStack, str) 
 }
 
-// Removing and returning top element of stack. Return false if stack is empty.
+
 func (newStack *Stack) Pop() (string, bool) {
 	if newStack.IsEmpty() {
 		return "", false
 	} else {
-		index := len(*newStack) - 1     // Index of the top most element.
-		element := (*newStack)[index]   // Index into the slice and obtain the element.
-		*newStack = (*newStack)[:index] // Removing it from the stack by slicing it off.
+		index := len(*newStack) - 1     
+		element := (*newStack)[index]   
+		*newStack = (*newStack)[:index] 
 		return element, true
 	}
 }
 
 func main() {
-	var stack Stack // Creating a stack variable of type Stack
+	var stack Stack
 
 	stack.Push("hi")
 	stack.Push("say")
