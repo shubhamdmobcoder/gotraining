866b1135b387c159f9dee0dc2b0a51a19b73e700
abhishek singh <abhishek.singh@mobcoder.com>
2021-09-06 17:29:35 +0530

    remove dup. & find targeted value from array

--- duplicate.go
+++ duplicate.go
@@ -6,18 +6,22 @@ func main() {
 	a := []int{100, 200, 300, 100, 200, 400, 500, 55, 0}
 	fmt.Println(a)
 
+	// test for method
 	x := remdup(a)
 	fmt.Println(x)
 }
 
 func remdup(a []int) []int {
+	//use map to record duplicates
 	y := map[int]bool{}
 	x := []int{}
 
 	for v := range a {
 		if y[a[v]] == true {
+			// here we don't add duplicates
 		} else {
 			y[a[v]] = true
+			//store or append the data into x
 			x = append(x, a[v])
 		}
 	}
--- target_array.go
+++ target_array.go
@@ -6,7 +6,7 @@ func main() {
 
 	fmt.Printf("Enter size of your array: ")
 	var size int
-	fmt.Scanln(&size)
+	fmt.Scanln(&size) // taking input(size of array) from user
 
 	var arr = make([]int, size)
 
@@ -21,6 +21,7 @@ func main() {
 
 	fmt.Println("Your array is: ", arr)
 
+	// iterate for adding element of array
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr); j++ {
 			if arr[i]+arr[j] == target {
