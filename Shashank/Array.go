74e8301a9ae7cbebc1929967019b8a655766c70c
mobcoder-shashank-kesarwani <shashank.kesarwani@mobcoder.com>
2021-09-06 15:55:31 +0530

    Queues in Go

--- Shashank/Array.go
+++ Shashank/Array.go
@@ -1,15 +1,7 @@
-package twoSum
+package main
 
-func twoSum(array []int, target int) []int {
-	for i := 0; i < len(array); i++ {
-		var start = array[i]
-		for j := i + 1; j < len(array); j++ {
-			var end = array[j]
-			if (start + end) == target {
-				return []int{i, j}
-			}
-		}
-	}
+import "fmt"
 
-	return []int{}
+func main() {
+	fmt.Println("Hello, World")
 }
